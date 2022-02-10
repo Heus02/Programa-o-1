a0a1e606dba0c2df2cbff76fa26ad1ab666980ef
Matheus Moreira Rodrigues <93472665+Heus02@users.noreply.github.com>
2022-02-10 07:52:44 -0300

    Add files via upload

---
+++ triangulo.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int classificacao (int tipo)
+{
+    int a, b, c;
+    if (a == b == c)
+    {
+        printf ("equilatero\n");
+    }
+    else ()
+    {
+        printf ("escaleno\n");
+    }
+
+    return 0;
+}
+int main() {
+    
+    int a, b, c;
+    
+    scanf ("%d%d%d", &a, &b, &c);
+    
+
+    printf ("\n", classificacao (tipo));
+        
+
+	return 0;
+}
